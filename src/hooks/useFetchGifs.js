e51137aec95d2f4eba1ecef4c761b4554e8f2eea
Edward Huertas <eahuertas@indracompany.com>
2024-09-13 01:49:45 -0500

    Primer Commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, SetIsLoading] = useState(true)
+
+    const getImages = async() => {
+        const newImages =  await getGifs(category);
+        setImages(newImages);
+        SetIsLoading(false);
+    }
+
+    useEffect( () => {
+        getImages();
+    }, []);
+
+  return {
+    images,
+    isLoading
+  }
+}
